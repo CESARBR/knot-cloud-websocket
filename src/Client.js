c4325269c00c385a1f62972379e3a53ffb64bb2c
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-29 02:58:49 -0300

    Add connect()/close() with authentication

---
+++ src/Client.js
@@ -0,0 +1,106 @@
+import EventEmitter from 'eventemitter3';
+import WebSocket from 'isomorphic-ws';
+import url from 'url';
+
+const PROXY_EVENTS = ['close', 'error', 'unexpected-response', 'ping', 'pong', 'open'];
+
+class Client extends EventEmitter {
+  constructor(options) {
+    super();
+    this.options = options;
+  }
+
+  connect() {
+    if (this.socket) {
+      this.socket.close();
+    }
+
+    const uri = this.buildUri();
+    this.socket = new WebSocket(uri);
+
+    const onOpen = () => {
+      const credentials = this.getCrendentials();
+      this.identity(credentials);
+      this.socket.removeEventListener('open', onOpen);
+    };
+
+    this.socket.addEventListener('message', this.onMessage.bind(this));
+    this.socket.addEventListener('open', onOpen);
+    PROXY_EVENTS.forEach(this.setupProxy.bind(this));
+  }
+
+  close() {
+    if (!this.socket) {
+      return;
+    }
+
+    this.closeAndClear();
+  }
+
+  identity(credentials) {
+    this.sendFrame('identity', credentials);
+  }
+
+  onMessage(event) {
+    const message = this.parseFrame(event.data);
+    if (message.type === 'error') {
+      const errorMessage = (message.data && message.data.message) || 'Unexpected error';
+      const error = new Error(errorMessage);
+      error.frame = event.data;
+      this.emit('error', error);
+      return;
+    }
+
+    this.emit(message.type, message.data);
+  }
+
+  sendFrame(type, data) {
+    const frame = this.formatFrame(type, data);
+    this.socket.send(frame);
+  }
+
+  closeAndClear() {
+    this.socket.close();
+    this.socket = null;
+  }
+
+  setupProxy(eventName) {
+    this.socket.addEventListener(eventName, (event) => {
+      this.emit(eventName, event);
+    });
+  }
+
+  buildUri() {
+    const urlOptions = {
+      hostname: this.options.hostname,
+      port: this.options.port,
+      pathname: '/ws',
+    };
+    urlOptions.protocol = urlOptions.port === 443 ? 'wss' : this.options.protocol || 'ws';
+
+    return url.format(urlOptions);
+  }
+
+  getCrendentials() {
+    return {
+      uuid: this.options.uuid,
+      token: this.options.token,
+    };
+  }
+
+  formatFrame(type, data) {
+    return JSON.stringify({ type, data });
+  }
+
+  parseFrame(frame) {
+    const message = JSON.parse(frame);
+    if (!message.type) {
+      const error = new Error('Invalid frame');
+      error.frame = frame;
+      throw error;
+    }
+    return message;
+  }
+}
+
+export default Client;
