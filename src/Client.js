13e9430110b5c47dcd262dce8811e14aabd64cad
João Neto <joao.alexandre@cesar.org.br>
2019-02-06 16:15:37 -0300

    Add set data
    Change-Id: I4d6d73e276d181e20ffefd1f37bde6714bc03024

--- src/Client.js
+++ src/Client.js
@@ -69,6 +69,10 @@ class Client extends EventEmitter {
     this.sendFrame('activate', { id });
   }
 
+  setData(id, data) {
+    this.sendFrame('setData', { id, data });
+  }
+
   onMessage(event) {
     const message = this.parseFrame(event.data);
     if (message.type === 'error') {
